7882207ef720499acad654351a3d299a574fc2fb
Yash Mehrotra <yashmehrotra95@gmail.com>
2014-11-28 18:21:29 +0530

    Finally Everything Working

--- garuda/garuda/views.py
+++ garuda/garuda/views.py
@@ -7,6 +7,7 @@ import hashlib
 import urllib2
 import requests
 import pdb
+#from __future__ import print_function
 
 from django.http import * #(HttpResponse, HttpResponseRedirect)
 from django.shortcuts import render, redirect
@@ -19,6 +20,10 @@ from settings import *
 def about(request):
     return render(request,'about.html')
 
+def test(request):
+    response = {'Hello':'yash'}
+    return HttpResponse(json.dumps(response),content_type = "application/json")
+
 def getDBObject():
     db = MySQLdb.connect(MYSQL_HOST,MYSQL_USERNAME,MYSQL_PASSWORD,'test')
     return db
@@ -30,32 +35,33 @@ def validate_user(user_email, user_password):
     try:
         db = getDBObject()
         cursor = db.cursor()
-        cursor.execute("SELECT user_id,user_handle FROM users WHERE user_email LIKE '%s' AND password LIKE '%s'" % (user_email, user_password))
+        cursor.execute("SELECT user_id,user_handle FROM users WHERE user_email LIKE '%s' AND user_password LIKE '%s'" % (user_email, user_password))
         row = cursor.fetchone()
         db.close()
+
+        if row:
+            user_id = str(row[0])
+            user_handle = str(row[1])
+            response = {
+                'status':'success',
+                'user_id':user_id,
+                'user_email':user_email,
+                'user_handle':user_handle
+            }
+        else:
+            response = { 
+                'status':'failed',
+                'reason':'username password combo doesnt match'    #Change the English
+            }
+
+        return response
 
     except MySQLdb.Error, e:
         errors.append(str(e))
 
-    if row:
-        user_id = str(row[0])
-        user_handle = str(row[1])
-        response = {
-            'status':'success',
-            'user_id':user_id,
-            'user_email':user_email,
-            'user_handle':user_handle
-        }
-    else:
-        response = { 
-            'status':'failed',
-            'reason':'username password combo doesnt match'    #Change the English
-        }
-
     if errors:
         response.append({'error':errors})
-
-    return response
+        return response
 
 def login_page(request):
     return render(request,'login.html')
@@ -93,7 +99,12 @@ def login_user(request):
             'status':'failed',
             'error':'not a post request'
         }
+
+    #pdb.set_trace()
+
+    #return HttpResponse(json.dumps(response), content_type = "application/json")
     return HttpResponse(json.dumps(response),content_type = "application/json")
+    #return HttpResponse("HEllo")
 
 @login_required
 def home_page(request):
@@ -101,7 +112,7 @@ def home_page(request):
     user_details = {
         'user_handle':request.session['user_handle'],
         'user_email':request.session['user_email'],
-        'tweets':get_user_tweets()
+        'tweets':get_user_tweets(request)
     }
     return render(request,'home.html',user_details)
 
@@ -126,7 +137,7 @@ def signup(request):
 
 @csrf_exempt
 def signupuser(request):
-    
+    #pdb.set_trace()
     if request.method == 'POST':
         user_email    = request.POST.get('user_email')
         user_name     = request.POST.get('user_name')
@@ -137,13 +148,17 @@ def signupuser(request):
         # Ethical
         user_password = hashlib.md5(user_password).hexdigest()
         errors = []
-        pdb.set_trace()
+        
         try:
             db = getDBObject()
             cursor = db.cursor()
             # Insert statement not working , Jasdeep to fix
-            sql_statement = "INSERT INTO users (user_email,user_name,user_password,user_handle,user_bio) VALUES ('%s', '%s', '%s', '%s', '%s') " % (str(user_email),str(user_name),str(user_password),str(user_handle),str(user_bio))
-            cursor.execute(sql_statement)
+            sql_statement = """INSERT INTO users (user_email,user_name,user_password,user_handle,user_bio) VALUES (%s, %s, %s, %s, %s) """
+
+            vals = (str(user_email),str(user_name),str(user_password),str(user_handle),str(user_bio))
+            #sql_statement = "INSERT INTO users (user_name) VALUES ('Yash M') "
+            #sql_statement = "INSERT users "
+            cursor.execute(sql_statement,vals)
         
             db.commit()
             db.close()
@@ -151,7 +166,10 @@ def signupuser(request):
         except MySQLdb.Error, e:
             errors.append(str(e))
 
+        #pdb.set_trace()
         user_valid = validate_user(user_email, user_password)
+
+        response = {}
         
         if user_valid['status'] == 'success':
             
@@ -179,24 +197,26 @@ def signupuser(request):
                 'reason':'Not a POST request'
             }
 
+    #pdb.set_trace()
+
     return HttpResponse(json.dumps(response),content_type = "application/json")
 
-def get_user_tweets():
+def get_user_tweets(request):
     
     user_handle = request.session['user_handle']
 
-    db = getDBObject()
-    cursor = db.cursor()
+    #db = getDBObject()
+    #cursor = db.cursor()
     #sql_statement = ""   -- SELECT tweets for the corresponding user_handle , or user_id 
-    cursor.execute(sql_statement)
-    row = cursor.fetchall()
-    db.close()
+    #cursor.execute(sql_statement)
+    #row = cursor.fetchall()
+    #db.close()
 
     tweets = []
 
-    for row in rows:
+    #for row in rows:
         # get tweets here
-        pass
+        #pass
 
     return tweets
     
