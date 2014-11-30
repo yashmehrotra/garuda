addba824eaa890d34dbb5e5cd8dc45f103a90821
Yash Mehrotra <yashmehrotra95@gmail.com>
2014-12-01 00:15:28 +0530

    One by one

--- garuda/garuda/views.py
+++ garuda/garuda/views.py
@@ -671,6 +671,6 @@ def view_all_users(request):
 
 def fill_tag_table(user_id, tag, tweet_id):
     ##
-    ##
+    ##Also insert the tags into the tags table , write an intelligent query which sees if tags are not there hence inserts other wise appends to a list of ids
     ##
     pass
